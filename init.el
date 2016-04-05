6a8ff09f51c1ccc9be5904eeedf264679d2df0b2
zim <zimuzostanley@gmail.com>
2016-04-05 11:50:53 +0100

    Markdown support

--- init.el
+++ init.el
@@ -215,3 +215,8 @@
 
 
 ;; Markdown mode
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
